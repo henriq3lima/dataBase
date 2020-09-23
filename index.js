e3392c0ba2b1723d69b7b970ec440b2175db7ee9
Henrique Lima <luizhenriquesilva@id.uff.br>
2020-09-23 12:07:02 -0300

    inserir scrap

--- index.js
+++ index.js
@@ -1,49 +1,19 @@
 const { SIGTERM } = require('constants');
 const express = require('express');
-const app = express();
 const http = require('http');
 
-//const livrosRouter = require('./livros/livros.js');
-//const gravarRouter = require('./BD/gravar.js');
-//const manipularRouter = require('./BD/manipular.js');
-//const manipularRouter = require('./BD/ler.js');
-/*
-livros router index livros
-BD/gravar router input arq.txt in BD
-BD/manipular return data select BD
-BD/ler print BD in page
-*/
+const sendRouter = require('./mongodb/send.js');
+const ytscrapRouter = require('./Youtube/scrap.js');
 
-//global.caminhoDados = 'dados/';
-//app.use('/livros',livrosRouter);
-//app.use('/gravar/BD',gravarRouter);
-//app.use('/manipular/BD',manipularRouter);
-const dados = [
-    {
-        info1: 1,
-        info2: 2
-    }, {
-        info1: 1,
-        info2: 2
-    }, {
-        info1: 1,
-        info2: 2
-    }, {
-        info1: 1,
-        info2: 2
-    }
-];
-app.listen(3000, function () {
-    console.log('A API está funcionando!');
+const app = express();
+
+app.listen(8000, function () {
+    console.log('A API está funcionando!')
 });
-http.createServer(function (request, response) {
-    response.setHeader("Access-Control-Allow-Origin", "*");
-    response.end(`
-        {
-            "info1": "1",
-            "info2": "2"
-        }`);
-}).listen(process.env.PORT || 8000)
+app.get('/', (_, res) => {
+    res.status(200).send('API-Banco NoSQL SQL')
+});
 
-// Imprime no console a URL de acesso ao servidor
-console.log('Servidor executando em http://127.0.0.1:8000/');
+
+app.use('/send', sendRouter);
+app.use('/ytscrap', ytscrapRouter);
