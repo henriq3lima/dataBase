0f7dca3607f36edbf8ac630ad9e8e2fca4adb429
Henrique Lima <luizhenriquesilva@id.uff.br>
2020-09-24 05:50:05 -0300

    ultimas tags pt.stack

--- Youtube/scrap.js
+++ Youtube/scrap.js
@@ -1,39 +1,38 @@
 //scrap you tube usando api deles
-const express = require('express'),
-    router = express.Router(),
-    fs = require('fs'),
-    request = require('request'),
-    cheerio = require('cheerio');
+const express = require("express"),
+  router = express.Router(),
+  request = require("request"),
+  cheerio = require("cheerio");
 
-router.get('/', (_, res) => {
+router.get("/", (_, res) => {
+  console.log("ok");
+  url = "https://pt.stackoverflow.com/";
+  request(url, function (error, _, html) {
+    if (!error) {
+      var $ = cheerio.load(html);
+      // Objeto que irá armazenar a tabela
+      var resultado1 = [];
+      var resultado2 = [];
+      $("#recent-tags-list > a").each(function (i) {
+        var assunto = $(this).text();
+        resultado1.push({
+          assunto: assunto,
+        });
+      });
 
-    console.log("ok");
-    url = 'https://viagemeturismo.abril.com.br/materias/os-100-lugares-mais-lindos-do-mundo/';
-    request(url, function (error, response, html) {
-        if (!error) {
-            var $ = cheerio.load(html);
-            // Objeto que irá armazenar a tabela
-            var resultado = [];
-            $('.slides > li > strong').each(function (i) {
-                // Obtendo as propriedades da tabela. 
-                // O método .trim() garante que irá remover espaço em branco
-                var titulo = $(this).text().trim();
-                // Inserindo os dados obtidos no nosso objeto
-                resultado.push({
-                    titulo: titulo
-                });
-            });
-            console.log(resultado);
-            async function f(res) {
-                await res;
-            };
-            res.setHeader("Access-Control-Allow-Origin", "*");
-            f(resultado).then(res.send(resultado));
+      $("#recent-tags-list > span").each(function (i) {
+        var cont = $(this).text();
+        resultado2.push({
+          cont: cont,
+        });
+      });
 
-        }
-
-    });
-
+      async function f(entrada1, entrada2) {
+        var saida1 = await entrada1;
+        var saida2 = await entrada2;
+      }
+      f(resultado1, resultado2).then(console.log(resultado1));
+    }
+  });
 });
 module.exports = router;
-
--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-const { SIGTERM } = require('constants');
-const express = require('express');
+const express = require("express");
 
-const sendRouter = require('./mongodb/send.js');
-const ytscrapRouter = require('./Youtube/scrap.js');
+const sendRouter = require("./mongodb/send.js");
+const ytscrapRouter = require("./Youtube/scrap.js");
 
 const app = express();
 
+app.use("/send", sendRouter);
+app.use("/ytscrap", ytscrapRouter);
+
 app.listen(process.env.PORT || 8000, function () {
-    console.log('A API está funcionando!')
-});
-app.get('/', (_, res) => {
-    res.status(200).send('API-Banco NoSQL SQL')
+  console.log("A API está funcionando!");
 });
 
-
-app.use('/send', sendRouter);
-app.use('/ytscrap', ytscrapRouter);
+app.get("/", (_, res) => {
+  res.status(200).send("API-Banco NoSQL SQL");
+});
