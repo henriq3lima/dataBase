2a2b1d645a751864126e55a4984b6f70f8256c67
Henrique Lima <luizhenriquesilva@id.uff.br>
2020-09-30 01:53:12 -0300

    teste heroku sem global3

--- mongodb/send.js
+++ mongodb/send.js
@@ -3,35 +3,35 @@ const express = require("express"),
 
 console.log("mongodb/send");
 router.get("/", (_, res) => {
-  res.status(200).send("Modulo de envio de scrap");
+  res.status(200).send("Modulo de envio de dados para o MongoDB");
 });
 
-// global.sendMongo = function sendMongo(dados) {
-//   const { MongoClient } = require("mongodb");
-//   console.log("functionSendMong");
+global.sendMongo = function sendMongo(dados) {
+  const { MongoClient } = require("mongodb");
+  console.log("functionSendMong");
 
-//   const mongodbURL =
-//     "mongodb+srv://CARAMELOAMARELO:H6395tQp7zKufkLf@2020um.yy4g6.gcp.mongodb.net/DADOSAMOSTRAGEM?retryWrites=true&w=majority";
-//   const client = new MongoClient(mongodbURL);
-//   const dbName = "2020UM";
-//   async function run() {
-//     try {
-//       await client.connect();
-//       console.log("Connected correctly to server");
-//       const db = client.db(dbName);
-//       const col = db.collection("questionPtStack");
-//       for (i = 0; i < dados.length; i++) {
-//         const p = await col.insertOne(dados[i]);
-//         const myDoc = await col.findOne();
-//       }
-//     } catch (err) {
-//       console.log(err.stack);
-//     } finally {
-//       await client.close();
-//     }
-//   }
+  const mongodbURL =
+    "mongodb+srv://CARAMELOAMARELO:H6395tQp7zKufkLf@2020um.yy4g6.gcp.mongodb.net/DADOSAMOSTRAGEM?retryWrites=true&w=majority";
+  const client = new MongoClient(mongodbURL);
+  const dbName = "2020UM";
+  async function run() {
+    try {
+      await client.connect();
+      console.log("Connected correctly to server");
+      const db = client.db(dbName);
+      const col = db.collection("questionPtStack");
+      for (i = 0; i < dados.length; i++) {
+        const p = await col.insertOne(dados[i]);
+        const myDoc = await col.findOne();
+      }
+    } catch (err) {
+      console.log(err.stack);
+    } finally {
+      await client.close();
+    }
+  }
 
-//   run().catch(console.dir);
-// };
+  run().catch(console.dir);
+};
 
 module.exports = router;
--- scrap/scrap.js
+++ scrap/scrap.js
@@ -4,45 +4,45 @@ const express = require("express"),
   cheerio = require("cheerio");
 
 router.get("/", (_, res) => {
-  //  scrapStackPt();
-  res.status(200).send("Modulo de envio de dados para o MongoDB");
+  scrapStackPt();
+  res.status(200).send("Modulo de scrap");
 });
 
-// global.scrapStackPt = function scrapStackPt() {
-//   var url = "https://pt.stackoverflow.com/tags";
-//   request(url, function (erro, response, html) {
-//     if (!erro) {
-//       async function loadData() {
-//         var $ = await cheerio.load(html);
-//         var tag = [];
-//         var contagem = [];
-//         var numero = [];
-//         var resultado = [];
+global.scrapStackPt = function scrapStackPt() {
+  var url = "https://pt.stackoverflow.com/tags";
+  request(url, function (erro, response, html) {
+    if (!erro) {
+      async function loadData() {
+        var $ = await cheerio.load(html);
+        var tag = [];
+        var contagem = [];
+        var numero = [];
+        var resultado = [];
 
-//         $(".s-card").each(function (i) {
-//           tag[i] = $(this).find(".grid--cell > a.post-tag").text();
-//           contagem[i] = $(this)
-//             .find(".mt-auto > div:nth-child(1)")
-//             .text()
-//             .replace(" perguntas", "");
-//           numero = i;
-//         });
-//         for (let i = 0; i <= numero; i++) {
-//           resultado[i] = {
-//             tag: tag[i],
-//             contagem: contagem[i],
-//             indice: i,
-//             data: Date(),
-//           };
-//         }
-//         return await resultado;
-//       }
+        $(".s-card").each(function (i) {
+          tag[i] = $(this).find(".grid--cell > a.post-tag").text();
+          contagem[i] = $(this)
+            .find(".mt-auto > div:nth-child(1)")
+            .text()
+            .replace(" perguntas", "");
+          numero = i;
+        });
+        for (let i = 0; i <= numero; i++) {
+          resultado[i] = {
+            tag: tag[i],
+            contagem: contagem[i],
+            indice: i,
+            data: Date(),
+          };
+        }
+        return await resultado;
+      }
 
-//       loadData().then((resultado) => {
-//         console.log(resultado);
-//       });
-//     }
-//   });
-// };
+      loadData().then((resultado) => {
+        console.log(resultado);
+      });
+    }
+  });
+};
 
 module.exports = router;
