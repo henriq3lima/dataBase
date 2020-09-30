97b1d2655409a9673443ca55cd1fdfe14d74a328
Henrique Lima <luizhenriquesilva@id.uff.br>
2020-09-30 01:37:41 -0300

    teste heroku sem global

--- scrap/scrap.js
+++ scrap/scrap.js
@@ -4,69 +4,44 @@ const express = require("express"),
   cheerio = require("cheerio");
 
 router.get("/", (_, res) => {
-  scrapStackPt();
+  //  scrapStackPt();
 });
 
-global.scrapStackPt = function scrapStackPt() {
-  var url = "https://pt.stackoverflow.com/tags";
-  request(url, function (erro, response, html) {
-    if (!erro) {
-      async function loadData() {
-        var $ = await cheerio.load(html);
-        var tag = [];
-        var contagem = [];
-        var numero = [];
-        var resultado = [];
+// global.scrapStackPt = function scrapStackPt() {
+//   var url = "https://pt.stackoverflow.com/tags";
+//   request(url, function (erro, response, html) {
+//     if (!erro) {
+//       async function loadData() {
+//         var $ = await cheerio.load(html);
+//         var tag = [];
+//         var contagem = [];
+//         var numero = [];
+//         var resultado = [];
 
-        $(".s-card").each(function (i) {
-          tag[i] = $(this).find(".grid--cell > a.post-tag").text();
-          contagem[i] = $(this)
-            .find(".mt-auto > div:nth-child(1)")
-            .text()
-            .replace(" perguntas", "");
-          numero = i;
-        });
-        for (let i = 0; i <= numero; i++) {
-          resultado[i] = {
-            tag: tag[i],
-            contagem: contagem[i],
-            indice: i,
-            //data: Date(),
-          };
-        }
-        return await resultado;
-      }
+//         $(".s-card").each(function (i) {
+//           tag[i] = $(this).find(".grid--cell > a.post-tag").text();
+//           contagem[i] = $(this)
+//             .find(".mt-auto > div:nth-child(1)")
+//             .text()
+//             .replace(" perguntas", "");
+//           numero = i;
+//         });
+//         for (let i = 0; i <= numero; i++) {
+//           resultado[i] = {
+//             tag: tag[i],
+//             contagem: contagem[i],
+//             indice: i,
+//             data: Date(),
+//           };
+//         }
+//         return await resultado;
+//       }
 
-      loadData().then((resultado) => {
-        sendMongo(resultado);
-      });
-    }
-  });
-};
+//       loadData().then((resultado) => {
+//         console.log(resultado);
+//       });
+//     }
+//   });
+// };
 
-/* 
-console.log("ok");
-url =
-  "https://viagemeturismo.abril.com.br/materias/os-100-lugares-mais-lindos-do-mundo/";
-request(url, function (error, response, html) {
-  if (!error) {
-    var $ = cheerio.load(html);
-    var resultado = [];
-
-    $(".slides > li > strong").each(function (i) {
-      var titulo = $(this).text().trim();
-
-      resultado.push({
-        titulo: titulo,
-      });
-    });
-
-    async function f(res) {
-      await res;
-    }
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    f(resultado).then(res.send(resultado));
-  }
-});
-*/
 module.exports = router;
