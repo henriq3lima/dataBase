86246721798e67ef03fbe8dba83bca785504ff41
Henrique Lima <luizhenriquesilva@id.uff.br>
2020-09-30 11:31:00 -0300

    retirada do cron1

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const CronJob = require("cron").CronJob;
+//const CronJob = require("cron").CronJob;
 
 const sendRouter = require("./mongodb/send.js");
 const scrapRouter = require("./scrap/scrap.js");
@@ -17,23 +17,23 @@ app.get("/", (_, res) => {
   res.status(200).send("API-Banco NoSQL SQL");
 });
 
-let dateStart = new Date("2020-09-30T08:00:00.000Z");
+// let dateStart = new Date("2020-09-30T15:00:00.000Z");
 
-const job = new CronJob(dateStart, function () {
-  inicial();
-});
-job.start();
+// const job = new CronJob(dateStart, function () {
+//   inicial();
+// });
+// job.start();
+// inicial();
+// function inicial() {
+//   var tagBr = "https://pt.stackoverflow.com/tags";
+//   var tagEn = "https://stackoverflow.com/tags";
 
-function inicial() {
-  var tagBr = "https://pt.stackoverflow.com/tags";
-  var tagEn = "https://stackoverflow.com/tags";
-
-  const trabalho = new CronJob("0 0 0/1 1/1 * *", function () {
-    const d = new Date();
-    console.log("Every second:", d);
-    scrapStack(tagEn);
-    scrapStack(tagBr);
-  });
-  console.log("After job instantiation");
-  trabalho.start();
-}
+//   const trabalho = new CronJob("0 0 0/1 1/1 * *", function () {
+//     const d = new Date();
+//     console.log("Every second:", d);
+//     scrapStack(tagEn);
+//     scrapStack(tagBr);
+//   });
+//   console.log("After job instantiation");
+//   trabalho.start();
+// }
